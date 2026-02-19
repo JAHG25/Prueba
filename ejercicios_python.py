5d3031d55cca5a01476197366d3886ecb06a9d69
Juan <juan54driver@gmail.com>
2026-02-19 17:00:06 -0600

    Se agrego un ejercicio mas

--- ejercicios_python.py
+++ ejercicios_python.py
@@ -196,3 +196,17 @@ else:
     grupo = "B"
 
 print(f"""Tu grupo es: {grupo}""")
+
+#Ejercicio 7
+renta_anual = int(input("Ingrese su renta anual: "))
+
+if renta_anual == 10000:
+  print("Su tipo de impositivo es de 5%")
+elif renta_anual > 10000 and renta_anual <= 20000:
+  print("Su tipo de impositivo es de 15%")
+elif renta_anual > 20000 and renta_anual <= 35000:
+  print("Su tipo de impositivo es de 20%")
+elif renta_anual > 35000 and renta_anual <= 60000:
+  print("Su tipo de impositivo es de 30%")
+else:
+  print("Su tipo de impositivo es de 45%")
