daf6f32c1c59a43bd45a0ee9a26a19ad6bd6b4bb
Juan <juan54driver@gmail.com>
2026-02-20 12:59:35 -0600

    Diccionarios

---
+++ Diccionarios.py
@@ -0,0 +1,11 @@
+#Nuevo diccionario
+
+DB = {
+    "pepe" : {"name" : "Pepe", "age" : 10},
+    "paco" : {"name" : "Paco", "age" : 12},
+    "luis" : {"name" : "Luis", "age" : 11},
+    "renata" : {"name" : "Renata", "age" : 10}
+}
+
+print(DB)
+print(DB["paco"])
